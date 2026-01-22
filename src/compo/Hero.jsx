a5636ec7d0eebe0cb2025bc81fc996f18480e44e
Usha Sai Kiran <twinklekiran69@gmail.com>
2026-01-22 15:45:22 -0800

    regula

--- src/compo/Hero.jsx
+++ src/compo/Hero.jsx
@@ -35,7 +35,7 @@ function Hero() {
             </span>
           </div>
 
-          <h2 className="role">Data Analyst</h2>
+          <h2 className="role"> a Data Analyst</h2>
 
           <p className="subtitle">
            A Data science practitioner and web developer who builds modern web apps,
